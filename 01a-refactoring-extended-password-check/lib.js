ea7df07f18c45eb5fa9a933fcbff9fda40898dc1
kiraGit67 <kirsten@sais.de>
2023-05-18 19:53:04 +0200

    feat: refactoring lib.js

--- 01a-refactoring-extended-password-check/lib.js
+++ 01a-refactoring-extended-password-check/lib.js
@@ -1,5 +1,13 @@
 "use strict";
 
+export function arePasswordsEqual(pw1, pw2) {
+  return pw1 === pw2;
+}
+
+export function atLeast10Chars(pw) {
+  return pw.length >= 10;
+}
+
 export function initApp() {
   const password1 = document.querySelector("#password1");
   const password2 = document.querySelector("#password2");
@@ -9,13 +17,17 @@ export function initApp() {
   const check10chars = document.querySelector("#check-ten-chars");
 
   function checkPasswords() {
-    if (password1.value === password2.value) {
+    const equalResult = arePasswordsEqual(password1.value, password2.value);
+
+    if (equalResult) {
       checkEqual.innerText = " ✅ ";
     } else {
       checkEqual.innerText = " ❌ ";
     }
 
-    if (password1.value.length >= 10) {
+    const checkGreaterEqual10 = atLeast10Chars(password1.value);
+
+    if (checkGreaterEqual10) {
       check10chars.innerText = " ✅ ";
     } else {
       check10chars.innerText = " ❌ ";
