aa843510070ebe98477eacf4a43bc0d86687cb63
kiraGit67 <kirsten@sais.de>
2023-05-18 19:41:12 +0200

    imports initApp() from lib.js into script.js

--- 01a-refactoring-extended-password-check/script.js
+++ 01a-refactoring-extended-password-check/script.js
@@ -1,28 +1,5 @@
 "use strict";
 
-function initApp() {
-  const password1 = document.querySelector("#password1");
-  const password2 = document.querySelector("#password2");
-  const checkButton = document.querySelector("#button");
-
-  const checkEqual = document.querySelector("#check-equal");
-  const check10chars = document.querySelector("#check-ten-chars");
-
-  function checkPasswords() {
-    if (password1.value === password2.value) {
-      checkEqual.innerText = " ✅ ";
-    } else {
-      checkEqual.innerText = " ❌ ";
-    }
-
-    if (password1.value.length >= 10) {
-      check10chars.innerText = " ✅ ";
-    } else {
-      check10chars.innerText = " ❌ ";
-    }
-  }
-
-  checkButton.addEventListener("click", checkPasswords);
-}
+import { initApp } from "./lib.js";
 
 initApp();
